29a59117d69918f8f70e5cfd1bc1e3c3e27bd592
Drewzlla <ohitzandrew@gmail.com>
2025-07-21 09:01:26 +0100

    basically done, minor issues with hero image and testimonial btn

--- main.js
+++ main.js
@@ -9,3 +9,39 @@
     toggleIcon.className = expanded ? "fas fa-times" : "fas fa-bars";
 
   });
+
+//   FAQ FUNCTION
+document.addEventListener('DOMContentLoaded', () => {
+    const faqContainer = document.querySelector('.faq-content');
+  
+    faqContainer.addEventListener('click', (e) => {
+      const groupHeader = e.target.closest('.faq-header');
+  
+      if (!groupHeader) return;
+  
+      const group = groupHeader.parentElement;
+      const groupBody = group.querySelector('.faq-body');
+      const icon = groupHeader.querySelector('i');
+  
+      // Toggle icon
+      icon.classList.toggle('fa-plus');
+      icon.classList.toggle('fa-minus');
+  
+      // Toggle visibility of body
+      groupBody.classList.toggle('open');
+  
+      // Close other open FAQ bodies
+      const otherGroups = faqContainer.querySelectorAll('.faq-group');
+  
+      otherGroups.forEach((otherGroup) => {
+        if (otherGroup !== group) {
+          const otherGroupBody = otherGroup.querySelector('.faq-body');
+          const otherIcon = otherGroup.querySelector('.faq-header i');
+  
+          otherGroupBody.classList.remove('open');
+          otherIcon.classList.remove('fa-minus');
+          otherIcon.classList.add('fa-plus');
+        }
+      });
+    });
+  });
